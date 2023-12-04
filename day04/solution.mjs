b15628310a8d0bcda6bdc2680bdc8a6953c6fdb5
Michal Pasierbski <mpasierbski@gmail.com>
2023-12-04 09:34:37 +0100

    day 4 - part 2 solution

--- day04/solution.mjs
+++ day04/solution.mjs
@@ -15,7 +15,7 @@ function part1() {
     const intersection = selected.filter((x) => new Set([...winning]).has(x));
     const points = intersection.reduce((acc, _, i) => {
       if (i < 2) return acc + 1;
-      return acc + (2**(i - 1));
+      return acc + 2 ** (i - 1);
     }, 0);
 
     return acc + points;
@@ -24,8 +24,44 @@ function part1() {
   return result;
 }
 
+function part2() {
+  const data = readFileSync("./day04/input2.txt", "utf-8");
+  const cards = data.split("\n");
+
+  function calculateCardHits(card) {
+    const [_, numbers] = card.split(":");
+    const [winning, selected] = numbers.split("|").map((part) => {
+      const matches = part.matchAll(/\d+/g);
+
+      return [...matches].map((match) => parseInt(match[0]));
+    });
+
+    return selected.filter((x) => new Set([...winning]).has(x)).length;
+  }
+
+  function processCard(index, scores = {}) {
+    let score = 1; // 1 for the card itself
+
+    // if we already calculated the score for this card, return it
+    if (scores[index] === undefined) {
+      scores[index] = calculateCardHits(cards[index]);
+    }
+
+    // process won cards
+    for (let i = 1; i <= scores[index]; i++) {
+      score += processCard(index + i, scores);
+    }
+
+    return score;
+  }
+
+  // process original cards
+  return cards.reduce((acc, _, i) => acc + processCard(i), 0);
+}
+
 async function main() {
-  console.log(part1());
+  //   console.log(part1());
+  console.log(part2());
 }
 
 main();

08163388209ecf12b02a12e4b24a809a027b3f79
Michal Pasierbski <mpasierbski@gmail.com>
2023-12-06 07:20:07 +0100

    Incomplete day 5 part 2 ;(

--- day05/solution.mjs
+++ day05/solution.mjs
@@ -1,6 +1,6 @@
 import { readFileSync } from "fs";
 
-function part1() {
+function main() {
   const data = readFileSync("./day05/input1.txt", "utf-8");
 
   const lines = data.split("\n");
@@ -30,29 +30,31 @@ function part1() {
   }
 
   let result = Infinity;
-  const seeds = [...lines[0].matchAll(/\d+/g)].map((match) =>
-    parseInt(match[0])
-  );
-  for (let seed of seeds) {
-    let value = seed;
+  let seeds = [...lines[0].matchAll(/\d+/g)].map((match) => parseInt(match[0]));
+  const visited = {};
 
-    for (let level of levels) {
-      for (let entry of level) {
-        if (entry.src <= value && entry.src + entry.range > value) {
-          value = entry.dest + (value - entry.src);
-          break;
+  for (let i = 0; i < seeds.length; i += 2) {
+    for (let j = 0; j < seeds[i + 1]; j++) {
+      let seed = seeds[i] + j;
+      let value = seed;
+
+      if (visited[seed]) continue;
+      visited[seed] = true;
+
+      for (let level of levels) {
+        for (let entry of level) {
+          if (entry.src <= value && entry.src + entry.range > value) {
+            value = entry.dest + (value - entry.src);
+            break;
+          }
         }
       }
-    }
 
-    result = Math.min(result, value);
+      result = Math.min(result, value);
+    }
   }
 
   return result;
-}
-
-function main() {
-  console.log(part1());
 }
 
 main();
